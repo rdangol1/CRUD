f55407d76e2b42651e8d6dfe2ac91dd76d1e48f0
rdangol1 <dridhisha@gmail.com>
2021-04-14 09:20:38 -0600

    rename again

---
+++ userAuthenticater/models/course.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose"),
+{Schema } = require("mongoose"),
+courseSchema = new Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+            unique: true
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        maxStudents:{
+            type: Number,
+            default:0,
+            min: [0, "Student can not have a negative number of students"]
+
+        },
+        cost:{
+            type: Number,
+            default:0,
+            min :[0, "Cost could not be a negative values"]
+
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model("Course", courseSchema);
