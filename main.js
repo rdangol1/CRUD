7f34c8bb82e5eab7245281f3a409ebf94340bc7c
rdangol1 <dridhisha@gmail.com>
2021-05-02 13:44:09 -0600

    changes

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-const express = require ("express"),
-app = express(),
-router = require("./routes/index"),
-homeController = require("./controllers/homecontroller"),
-errorController = require("./controllers/errorController"),
-subscribersController =  require("./controllers/subscribersController"),
-coursesController =  require("./controllers/coursesController"),
-usersController =  require("./controllers/usersController"),
-methodOverride = require("method-override"),
-expressSession = require("express-session"),
-cookieParser = require("cookie-parser"),
-connectFlash = require("connect-flash"),
-expressValidator = require("express-validator"),
-bodyParser = require("body-parser"),
-passport = require("passport"),
-layouts = require("express-ejs-layouts"), mongoose = require("mongoose") ;
-Subscriber = require("./models/subscriber");
-Course = require("./models/course");
-User = require("./models/user");
+"use strict";
 
+const express = require("express"),
+  app = express(),
+  router = require("./routes/index"),
+  homeController = require("./controllers/homecontroller"),
+  errorController = require("./controllers/errorController"),
+  subscribersController =  require("./controllers/subscribersController"),
+  coursesController =  require("./controllers/coursesController"),
+  usersController =  require("./controllers/usersController"),
+  methodOverride = require("method-override"),
+  expressSession = require("express-session"),
+  cookieParser = require("cookie-parser"),
+  connectFlash = require("connect-flash"),
+  expressValidator = require("express-validator"),
+  bodyParser = require("body-parser"),
+  passport = require("passport"),
+  layouts = require("express-ejs-layouts"),
+  mongoose = require("mongoose"),
+  Subscriber = require("./models/subscriber"),
+  Course = require("./models/course"),
+  User = require("./models/user");
+
+mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://localhost:27017/confetti_cuisine", { useNewUrlParser: true  });
 //mongoose.createConnection("mongodb://localhost:27017/confetti_cuisine", { useNewUrlParser: true, useUnifiedTopology: true  });
-
-mongoose.Promise = global.Promise;
 
 mongoose.set("useCreateIndex", true);
 
@@ -32,26 +34,36 @@ db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 });
 app.set("port", process.env.PORT || 3000);
-
 app.set("view engine", "ejs");
 
-router.use(layouts);
+app.use(layouts);
+app.use(
+  express.urlencoded({
+      extended:false
+  })
+);
+
+
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
+
+// router vs app
+app.use(express.json());
+
 router.use(expressValidator());
 app.use(bodyParser.urlencoded({
     extended: true
   }));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-router.use(
-    express.urlencoded({
-        extended:false
-    })
-)
-router.use(express.json());
 
-router.use(cookieParser("my_passcode"));
-router.use(
+
+app.use(cookieParser("my_passcode"));
+app.use(
   expressSession({
     secret: "my_passcode",
     cookie: {
@@ -61,17 +73,18 @@ router.use(
     saveUninitialized: false
   }));
 
-router.use(passport.initialize());
-router.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-router.use(connectFlash());
+app.use(connectFlash());
 
-router.use((req, res, next) => {
-  res.locals.flashMessages = req.flash();
+app.use((req, res, next) => {
+  
   res.locals.loggedIn = req.isUnauthenticated();
   res.locals.currentUser = req.user;
+  res.locals.flashMessages = req.flash();
   next();
 });
 
